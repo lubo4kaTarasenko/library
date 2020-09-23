a605d9d4bb89cadb2b06334c3b16072bf0649ccb
luba.t <luba@t.me>
2020-09-23 16:30:05 +0300

    add title validation & update function to admin

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,19 +4,37 @@ class BooksController < ApplicationController
   end
 
   def new
+    @book = Book.new
   end
 
-def create
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def show
+    @book = Book.find(params[:id])
+  end
+
+  def create
   @book = Book.new(book_params)
- 
-  @book.save
-  redirect_to @book
+
+  if @book.save
+    redirect_to @book
+  else
+    render 'new'
+  end
 end
 
-def show
+def update
   @book = Book.find(params[:id])
+ 
+  if @book.update(book_params)
+    redirect_to @book
+  else
+    render 'edit'
+  end
 end
-
 
 private
   def book_params
