48d3f20734547de6fe487d9c474a949f6fe08235
luba.t <luba@t.me>
2020-09-26 17:55:15 +0300

    add cover resize

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,5 @@
 class BooksController < ApplicationController
-
+  
   def index
     @books = Book.all
   end
@@ -18,34 +18,39 @@ class BooksController < ApplicationController
   end
 
   def create
-  @book = Book.new(book_params)
+    @book = Book.new(book_params)
 
-  if @book.save
-    redirect_to @book
-  else
-    render 'new'
+    if @book.save
+      redirect_to @book
+    else
+      render 'new'
+    end
   end
-end
 
-def update
-  @book = Book.find(params[:id])
- 
-  if @book.update(book_params)
-    redirect_to @book
-  else
-    render 'edit'
+  def update
+    @book = Book.find(params[:id])
+  
+    if @book.update(book_params)
+      redirect_to @book
+    else
+      render 'edit'
+    end
   end
-end
 
-def destroy
-  @book = Book.find(params[:id])
-  @book.destroy
- 
-  redirect_to books_path
-end
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+  
+    redirect_to books_path
+  end
 
-private
+  private
   def book_params
-    params.require(:book).permit(:title, :author, :cover)
+    if params.dig(:book, :cover)
+      image = MiniMagick::Image.new(params[:book][:cover].tempfile.path)
+      image.resize "50x50"
+    end
+
+    params.require(:book).permit(:title, :author, :cover, :content)
   end
 end
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,7 +1,9 @@
 class Book < ApplicationRecord
+  
   has_one_attached :cover
   has_one_attached :content
 
   validates :title, presence: true,
                     length: { minimum: 2 }
+
 end
