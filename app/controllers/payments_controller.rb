508aa55f6bcb4057b5c80fa477af28b4d9438766
luba.t <luba@t.me>
2020-10-05 16:56:25 +0300

    connect user to book

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,4 +1,9 @@
 class PaymentsController < ApplicationController
-  def create
+  def create    
+    @book = Book.find(params[:book_id])
+    #a = @book.user_books.create(user_id: current_user)  
+    a = @book.users << current_user
+    puts a.inspect
+    
   end
 end
